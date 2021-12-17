06b1b1ebb70ac566d952823e452017b6faa19c43
Yiheng <48312380+maakabaka@users.noreply.github.com>
2021-12-17 14:06:00 +0800

    Add files via upload

---
+++ download_data_weights.sh
@@ -0,0 +1,8 @@
+
+wget --no-check-certificate https://filebox.ece.vt.edu/~chengao/FGVC/data.zip
+unzip data.zip
+rm data.zip
+
+wget --no-check-certificate https://filebox.ece.vt.edu/~chengao/FGVC/weight.zip
+unzip weight.zip
+rm weight.zip
